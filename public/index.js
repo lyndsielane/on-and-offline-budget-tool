e73b5ad3ff7afefb12d7510c474bb1b1cc77a836
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-16 21:39:40 -0400

    service worker under construction

--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
 let transactions = [];
 let myChart;
 
+if ("serviceWorker" in navigator) {
+  let registration;
+
+  const registerServiceWorker = async () => {
+    registration = await navigator.serviceWorker.register(
+      "./service-worker.js"
+    );
+  };
+
+  registerServiceWorker();
+}
+
 fetch("/api/transaction")
   .then((response) => {
     return response.json();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [
+const cacheName = "budget-cache";
+
+// put the static assets and routes you want to cache here
+const filesToCache = [
   "/",
   "models/transaction.js",
   "/icons/icon-192x192.png",
@@ -13,73 +14,23 @@ const FILES_TO_CACHE = [
   "server.js",
 ];
 
-// install
-self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
+// the event handler for the activate event
+self.addEventListener("activate", (e) => self.clients.claim());
 
-  // pre cache all static assets
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+// the event handler for the install event
+// typically used to cache assets
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
   );
-
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
-  self.skipWaiting();
 });
 
-// activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-// fetch
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-
-    return;
-  }
-
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
+// the fetch event handler, to intercept requests and serve all
+// static assets from the cache
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((response) => (response ? response : fetch(e.request)))
   );
 });
