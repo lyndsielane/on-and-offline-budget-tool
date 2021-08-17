8dc1fc21f567b57ead80a67999d84410d91c4828
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-17 16:44:18 -0400

    fixed 404 error

--- public/index.js
+++ public/index.js
@@ -1,26 +1,11 @@
 let transactions = [];
 let myChart;
 
-if ("serviceWorker" in navigator) {
-  let registration;
-
-  const registerServiceWorker = async () => {
-    registration = await navigator.serviceWorker.register(
-      "./service-worker.js"
-    );
-  };
-
-  registerServiceWorker();
-}
-
 fetch("/api/transaction")
-  .then((response) => {
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((data) => {
     // save db data on global variable
     transactions = data;
-
     populateTotal();
     populateTable();
     populateChart();
@@ -28,21 +13,23 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
+  const total = transactions
+    .reduce((total, t) => {
+      return total + parseFloat(t.value);
+    }, 0)
+    .toFixed(2);
 
-  let totalEl = document.querySelector("#total");
+  const totalEl = document.querySelector("#total");
   totalEl.textContent = total;
 }
 
 function populateTable() {
-  let tbody = document.querySelector("#tbody");
+  const tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
   transactions.forEach((transaction) => {
     // create and populate a table row
-    let tr = document.createElement("tr");
+    const tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${transaction.name}</td>
       <td>${transaction.value}</td>
@@ -54,17 +41,17 @@ function populateTable() {
 
 function populateChart() {
   // copy array and reverse it
-  let reversed = transactions.slice().reverse();
+  const reversed = transactions.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
-  let labels = reversed.map((t) => {
-    let date = new Date(t.date);
+  const labels = reversed.map((t) => {
+    const date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  let data = reversed.map((t) => {
+  const data = reversed.map((t) => {
     sum += parseInt(t.value);
     return sum;
   });
@@ -74,7 +61,7 @@ function populateChart() {
     myChart.destroy();
   }
 
-  let ctx = document.getElementById("myChart").getContext("2d");
+  const ctx = document.getElementById("my-chart").getContext("2d");
 
   myChart = new Chart(ctx, {
     type: "line",
@@ -93,9 +80,9 @@ function populateChart() {
 }
 
 function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
+  const nameEl = document.querySelector("#t-name");
+  const amountEl = document.querySelector("#t-amount");
+  const errorEl = document.querySelector("form .error");
 
   // validate form
   if (nameEl.value === "" || amountEl.value === "") {
@@ -106,7 +93,7 @@ function sendTransaction(isAdding) {
   }
 
   // create record
-  let transaction = {
+  const transaction = {
     name: nameEl.value,
     value: amountEl.value,
     date: new Date().toISOString(),
@@ -134,9 +121,7 @@ function sendTransaction(isAdding) {
       "Content-Type": "application/json",
     },
   })
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
       if (data.errors) {
         errorEl.textContent = "Missing Information";
@@ -148,6 +133,7 @@ function sendTransaction(isAdding) {
     })
     .catch((err) => {
       // fetch failed, so save in indexed db
+      console.log("save record");
       saveRecord(transaction);
 
       // clear form
@@ -156,10 +142,12 @@ function sendTransaction(isAdding) {
     });
 }
 
-document.querySelector("#add-btn").onclick = function () {
+document.querySelector("#add-btn").addEventListener("click", function (event) {
+  event.preventDefault();
   sendTransaction(true);
-};
+});
 
-document.querySelector("#sub-btn").onclick = function () {
+document.querySelector("#sub-btn").addEventListener("click", function (event) {
+  event.preventDefault();
   sendTransaction(false);
-};
+});
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,36 +1,69 @@
-const cacheName = "budget-cache";
-
-// put the static assets and routes you want to cache here
-const filesToCache = [
+const FILES_TO_CACHE = [
   "/",
   "models/transaction.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "/index.html",
-  "/index.js",
+  "/js/index.js",
+  "/js/indexedDb.js",
+  "/js/db.js",
   "/manifest.webmanifest",
-  "/styles.css",
-  "routes/api.js",
+  "/service-worker.js",
+  "/css/styles.css",
+  "/routes/api.js",
   "server.js",
 ];
 
-// the event handler for the activate event
-self.addEventListener("activate", (e) => self.clients.claim());
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
-// the event handler for the install event
-// typically used to cache assets
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
-// the fetch event handler, to intercept requests and serve all
-// static assets from the cache
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
     caches
-      .match(e.request)
-      .then((response) => (response ? response : fetch(e.request)))
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
