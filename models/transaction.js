3b66c495c6ab41ed42a2a59f29a0062a81260ab6
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-18 10:16:34 -0400

    db fix

--- models/transaction.js
+++ models/transaction.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
+const TransactionSchema = new Schema({
   name: {
     type: String,
     trim: true,
