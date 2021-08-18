5b5aab901c39ec57cd8498f55c51d5768f22d467
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-18 11:21:27 -0400

    indexjs fix

--- models/transaction.js
+++ models/transaction.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const TransactionSchema = new Schema({
+const transactionSchema = new Schema({
   name: {
     type: String,
     trim: true,
@@ -18,6 +18,6 @@ const TransactionSchema = new Schema({
   },
 });
 
-const Transaction = mongoose.model("Transaction", TransactionSchema);
+const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
